bd392087898ac4445bcf07c2eedbcf97208219f8
bhuradiaarchit <abhuradia15@gmail.com>
2026-02-04 13:27:40 +0000

    fix:Creating service for live data fetching

--- src/main/java/com/asish/portfolio_investment/Service/MarketDataService.java
+++ src/main/java/com/asish/portfolio_investment/Service/MarketDataService.java
@@ -1,4 +1,29 @@
+
 package com.asish.portfolio_investment.Service;
 
+
+
+import com.asish.portfolio_investment.dto.MarketPriceResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class MarketDataService {
+
+    private static final String PYTHON_BASE_URL = "http://localhost:5000";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public MarketPriceResponseDTO getLivePrice(String symbol) {
+        try {
+            String url = PYTHON_BASE_URL + "/market/live?symbol=" + symbol;
+            return restTemplate.getForObject(url, MarketPriceResponseDTO.class);
+        } catch (RestClientException ex) {
+            throw new RuntimeException("Live market data service unavailable");
+        }
+    }
 }
